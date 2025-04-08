32062817cccdb95e3d158eca84880e3eb52d8221
mahmoud mohamed <mahmoudkeshar52@gmail.com>
2025-04-08 23:52:47 +0200

    update Dashbord code

---
+++ Estigo/DTO/SubmitExamResultDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Estigo.DTO
+{
+    public class SubmitExamResultDTO
+    {
+        public string StudentId { get; set; }
+        public int ExamId { get; set; }
+        public double Score { get; set; }
+    }
+}
